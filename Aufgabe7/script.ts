160db6a319cddc400e7715bf8d562a6de618a94d
Felix Lütte <felix.luette@web.de>
2020-06-17 10:17:02 +0200

    Json fetch

--- Aufgabe7/script.ts
+++ Aufgabe7/script.ts
@@ -4,7 +4,7 @@ namespace Aufgabe7 {
 export let products: Product[] = [];
 
 //getting the json
-getProductsJson("http://127.0.0.1:5500/Aufgabe7/products.json");
+// getProductsJson("http://127.0.0.1:5500/Aufgabe7/products.json");
 
 //product inizilatzation
 export function initialization(): void {
@@ -13,6 +13,7 @@ export function initialization(): void {
 }
 
 window.addEventListener("load", function(): void {
+    getProductsJson("http://127.0.0.1:5500/Aufgabe7/https://flexilue.github.io/GIS_SoSe_2020/Aufgabe7/products.json");
     buildBuyCircle();
     updatePrice();
 });
