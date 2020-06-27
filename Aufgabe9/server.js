2532e9ad2bde96e1ed4a1fe96b55bd9a81c479d0
Flexi <felix.luette@web.de>
2020-06-27 20:25:56 +0200

    Aufgabe9

---
+++ Aufgabe9/server.js
@@ -0,0 +1,49 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.A09Server = void 0;
+const Http = require("http");
+const Url = require("url");
+var A09Server;
+(function (A09Server) {
+    //Konsolen Ausgabe
+    console.log("Starting server");
+    //Vaiablen definition port mit dem Port des Processes 
+    let port = Number(process.env.PORT);
+    //Überprüft ob Port einen Wert hat, wenn nicht wird ein standard Port zugewiesen
+    if (!port)
+        port = 8100;
+    //Server Variable wird definiert und ein neuer Server wird dieser zugewiesen
+    let server = Http.createServer();
+    //Der server setzt einen Listener "request" und führt gegebnenfalls handleRequest aus
+    server.addListener("request", handleRequest);
+    //Der server setzt einen Listener "listening" und führt gegebnenfalls handleRequest aus
+    server.addListener("listening", handleListen);
+    //Server hört auf den zuvor definierten Port
+    server.listen(port);
+    //Consolen Aufgabe "Listening" bei Aufruf der Mehtode
+    function handleListen() {
+        console.log("Listening");
+    }
+    function handleRequest(_request, _response) {
+        //Consolenausgabe "I hear voices!"
+        console.log("I hear voices!");
+        //Setzt den Header der HTTP Request
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        if (_request.url) {
+            let url = Url.parse(_request.url, true);
+            let path = url.pathname;
+            if (path == "/html") {
+                for (let key in url.query) {
+                    _response.write(key + ": " + url.query[key] + "<br/>");
+                }
+            }
+            else if (path == "/json") {
+                let jsonString = JSON.stringify(url.query);
+                _response.write(jsonString);
+            }
+        }
+        _response.end();
+    }
+})(A09Server = exports.A09Server || (exports.A09Server = {}));
+//# sourceMappingURL=server.js.map
---
+++ Aufgabe9/server.ts
@@ -0,0 +1,50 @@
+import * as Http from "http";
+import * as Url from "url";
+
+export namespace A09Server {
+    //Konsolen Ausgabe
+    console.log("Starting server");
+    //Vaiablen definition port mit dem Port des Processes 
+    let port: number = Number(process.env.PORT);
+    //Überprüft ob Port einen Wert hat, wenn nicht wird ein standard Port zugewiesen
+    if (!port)
+        port = 8100;
+    
+    //Server Variable wird definiert und ein neuer Server wird dieser zugewiesen
+    let server: Http.Server = Http.createServer();
+    //Der server setzt einen Listener "request" und führt gegebnenfalls handleRequest aus
+    server.addListener("request", handleRequest);
+    //Der server setzt einen Listener "listening" und führt gegebnenfalls handleRequest aus
+    server.addListener("listening", handleListen);
+    //Server hört auf den zuvor definierten Port
+    server.listen(port);
+
+    //Consolen Aufgabe "Listening" bei Aufruf der Mehtode
+    function handleListen(): void {
+        console.log("Listening");
+    }
+
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        //Consolenausgabe "I hear voices!"
+        console.log("I hear voices!");
+
+        //Setzt den Header der HTTP Request
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+
+        if (_request.url) {
+            let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+            let path: string | null = url.pathname;
+            if (path == "/html") {
+              for (let key in url.query) {
+                _response.write(key + ": " + url.query[key] + "<br/>");
+              }
+            }
+            else if (path == "/json") {
+              let jsonString: string = JSON.stringify(url.query);
+              _response.write(jsonString);
+            }
+          }
+        _response.end();
+    }
+}
